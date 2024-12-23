3b507cea4f11666939dc1bceaddfb28bb3293d4d
user-onion-japan-1919 <shuta.12.27st@gmail.com>
2024-12-23 12:52:36 +0900

    postsコントローラー作成

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,13 +1,18 @@
 class PostsController < ApplicationController
   def index
-    @posts = Post.all
-  end
-
-  def new
-  end
-
-  def create
-    Post.create(content: params[:content])
-    redirect_to "/posts"
   end
 end
+
+
+
+#def index
+#@posts = Post.all
+#end
+
+#def new
+#end
+
+#def create
+#Post.create(content: params[:content])
+#redirect_to "/posts"
+#end
